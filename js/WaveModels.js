32461dee1e6616197e168233849d4ec0e24ac4a2
Rasim Labibov <postrediori@gmail.com>
2019-07-08 14:32:57 +0300

    Move math utils to a separate file

--- js/WaveModels.js
+++ js/WaveModels.js
@@ -1,20 +1,4 @@
 
-function CartesianToRadial(coord, origin) {
-    var dX = coord.x - origin.x;
-    var dY = coord.y - origin.y;
-    
-    var theta = Math.atan2(dY, dX);
-    var r = Math.sqrt(dX * dX + dY * dY);
-    
-    return {theta: theta, r: r};
-}
-
-function DistanceBetweenPoints(p1, p2) {
-    var dX = p1.x - p2.x;
-    var dY = p1.y - p2.y;
-    return Math.sqrt(dX * dX + dY * dY);
-}
-
 ModelParameters = function(data) {
     this.fps = parseInt(data["fps"].value);
     this.pointsCount = parseInt(data["points-count"].value);
